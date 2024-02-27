00c036c0d5fa8cb4c18a4d8436c4af17b46e93a4
DouglasSantos777 <fdsantosdec@aluno.uespi.br>
2024-02-27 14:23:51 -0300

    add design to the login page

--- src/pages/loginpage/Loginpage.tsx
+++ src/pages/loginpage/Loginpage.tsx
@@ -3,7 +3,30 @@ import '../loginpage/Loginpage.css'
 
 const LoginPage = () => {
   return (
-    <div>Login Page</div>
+    <div class="container">
+        <div class="left">
+            <div class="background"></div>
+            <img src="img/neoboard_logo.png" alt="NeoBoard logo" class="logo">
+            <img src="img/loginImg.png" alt="img login" class="left-image">
+        </div>
+        <div class="right">
+            <h2 class="title">Seja Bem-Vindo Novamente</h2>
+            <h3 class="subtitle">Preencha os campos corretamente</h3>
+            <form>
+                <div class="input-field">
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" placeholder="example@gmail.com" class="email">
+                </div>
+                <div class="input-field">
+                    <label for="senha">Senha:</label>
+                    <input type="password" name="senha" placeholder="senha" class="password">
+                </div>
+                <button type="submit" class="login-btn">Logar</button>
+            </form>
+            <p class="singup-text">Ainda não tem conta? <a href="#">Cadastre-se</a></p>
+            <button class="login-google">Login com o Google</button>
+        </div>
+    </div>
   )
 }
 
