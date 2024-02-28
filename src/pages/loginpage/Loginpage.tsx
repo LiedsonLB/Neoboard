83299df33c2a737f70ebcd790034e4855f159069
DouglasSantos777 <fdsantosdec@aluno.uespi.br>
2024-02-27 23:26:55 -0300

    feat: add login page design improvements

--- src/pages/loginpage/Loginpage.tsx
+++ src/pages/loginpage/Loginpage.tsx
@@ -8,30 +8,38 @@ const LoginPage = () => {
 
 
   return (
+    <><div className="background-color"></div>
     <div className="container">
         <div className="left">
-            <div className="background"></div>
-            <img src="neoboardCopia\public\img/readme_img/neoboard_logo.png" alt="NeoBoard logo" className="logo"/>
+            <div className="container-logo">
+            <img src="img/icon_neoboard.png" alt="NeoBoard logo" className="logo"/>
+            <p className="logo-name">NeoBoard</p>
+            </div>
             <img src="img/loginImg.png" alt="img login" className="left-image"/>
         </div>
         <div className="right">
             <h2 className="title">Seja Bem-Vindo Novamente</h2>
-            <h3 className="subtitle">Preencha os campos corretamente</h3>
-            <form>
-                <div className="input-field">
-                    <label>Email:</label>
-                    <input type="email" name="email" placeholder="example@gmail.com" className="email"/>
+            <p className="subtitle">Preencha os campos corretamente</p>
+            <div className="login-field">
+                <form>
+                    <div className="input-field">
+                        <label>Email:</label>
+                        <input type="email" name="email" placeholder="example@gmail.com" className="email"/>
+                    </div>
+                    <div className="input-field">
+                        <label>Senha:</label>
+                        <input type="password" name="senha" placeholder="senha" className="password"/>
+                    </div>
+                    <button type="submit" className="login-btn">Logar</button>
+                </form>
+                <div className="container-line">
+                    <hr id="line"/>
+                    <p className="singup-text">Ainda não tem conta? <a href="#">Cadastre-se</a></p>
                 </div>
-                <div className="input-field">
-                    <label>Senha:</label>
-                    <input type="password" name="senha" placeholder="senha" className="password"/>
-                </div>
-                <button type="submit" className="login-btn">Logar</button>
-            </form>
-            <p className="singup-text">Ainda não tem conta? <a href="#">Cadastre-se</a></p>
-            <button className="login-google">Login com o Google</button>
+                <button className="login-google">Login com o Google</button>
+            </div>
         </div>
-    </div>
+    </div></>
   )
 }
 
