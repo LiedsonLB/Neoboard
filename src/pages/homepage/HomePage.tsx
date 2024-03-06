f7020636f5fc727937e00fa75f4ffa76495dfc22
LiedsonLB <liedson.b9@gmail.com>
2024-03-06 10:15:39 -0300

    feat: update img e struture cadaster

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
   const [user, loading] = useAuthState(auth);
 
   useEffect(() => {
-    document.title = `NeoBoard | ${user?.displayName?.split(" ")[0] || "Home"}`;
+    document.title = `NeoBoard | ${user?.displayName || "Home"}, bem vindo!!`;
   }, [user]);
 
   useEffect(() => {
