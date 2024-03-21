e80b4aa0ac5915be97ce68c20847320431c666e7
DouglasSantos777 <fdsantosdec@aluno.uespi.br>
2024-03-20 22:24:38 -0300

    Co-authored-by: Kaio <KaioSimeao@users.noreply.github.com>

--- src/components/aside/Aside.tsx
+++ src/components/aside/Aside.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { IoGridOutline, IoBasketOutline, IoLocationOutline, IoPeopleOutline, IoBarChartOutline, IoDocumentTextOutline, IoPaperPlaneOutline, IoLogOutOutline } from 'react-icons/io5';
+import { IoGridOutline, IoBasketOutline, IoLocationOutline, IoPeopleOutline, IoBarChartOutline, IoDocumentTextOutline, IoPaperPlaneOutline, IoLogOutOutline, IoMenuOutline, IoCloseOutline, IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 import { signOut } from 'firebase/auth'
 // @ts-ignore
 import { auth } from '../../services/firebase'
@@ -10,10 +10,11 @@ interface Props {
     changeComponent: (component: string) => void;
 }
 
-const Aside = ( {user, changeComponent}: Props ) => {
+const Aside = ({ user, changeComponent }: Props) => {
     const [photo, setPhoto] = useState("");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [sidebarOpen, setSidebarOpen] = useState(true);
 
     useEffect(() => {
         if (user) {
@@ -21,76 +22,87 @@ const Aside = ( {user, changeComponent}: Props ) => {
             setEmail(user.email ?? "");
             setPhoto(user.photoURL ?? "img/no_profile.png");
         }
-    }, [user])
+    }, [user]);
 
-  return (
-    <aside id="container-home">
-        <header>
-            <img src="img/icon_neoboard.png" alt='titulo NeoBoard' />
-            <span id="title-home">
-                <h2>NeoBoard</h2>
-            </span>
-        </header>
-        
-        <div className='user-acount' style={{display: "flex"}} data-toggle="tooltip" title={email}>
-            <img id='acount-photo' src={photo} alt="photoUser" />
-            <div id='acount-info'>
-                <h4 id='acount-userName'>{name}</h4>
-                <p id='acount-email'>{email}</p>
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
+    return (
+        <main id='aside-container'>
+            <div id='togle-btn'>
+                <button id="toggleSidebarBtn" onClick={toggleSidebar}>
+                    {sidebarOpen ? <IoCloseOutline /> : <IoMenuOutline />}
+                </button>
             </div>
-            <button onClick={() => signOut(auth)}><IoLogOutOutline /></button>
-        </div>
-        <div id="description">
-            <p>Informe-se sobre:</p>
-        </div>
-        <nav id="navigation-home">
-            <ul>
-                <li onClick={() => changeComponent('Dashboard')}>
-                        <span className="icon"><IoGridOutline /></span>
-                        <span className="title-home">
-                            <h4>DashBoard</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('Produtos')}>
-                        <span className="icon"><IoBasketOutline /></span>
-                        <span className="title-home">
-                            <h4>Produtos</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('Regioes')}>
-                        <span className="icon"><IoLocationOutline /></span>
-                        <span className="title-home">
-                            <h4>Regiões</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('Funcionarios')}>
-                        <span className="icon"><IoPeopleOutline /></span>
-                        <span className="title-home">
-                            <h4>Funcionários</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('Financeiro')}>
-                        <span className="icon"><IoBarChartOutline /></span>
-                        <span className="title-home">
-                            <h4>Financeiro</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('Relatorio')}>
-                        <span className="icon"><IoDocumentTextOutline /></span>
-                        <span className="title-home">
-                            <h4>Enviar Relatório</h4>
-                        </span>
-                </li>
-                <li onClick={() => changeComponent('FAQ')}>
-                    <span className="icon"><IoPaperPlaneOutline/></span>
-                    <span className="title-home">
-                        <h4>Fale conosco</h4>
+            <aside id="aside-home" className={sidebarOpen ? '' : 'closed'}>
+                <header id='logo-aside'>
+                    <img src="img/icon_neoboard.png" alt='titulo NeoBoard' />
+                    <span id="title-home">
+                        <h2>NeoBoard</h2>
                     </span>
-                </li >
-            </ul>
-        </nav>
-    </aside>
-  )
+                </header>
+
+                <div className='user-acount' style={{ display: "flex" }} data-toggle="tooltip" title={email}>
+                    <img id='acount-photo' src={photo} alt="photoUser" />
+                    <div id='acount-info'>
+                        <h4 id='acount-userName'>{name}</h4>
+                        <p id='acount-email'>{email}</p>
+                    </div>
+                    <button onClick={() => signOut(auth)}><IoLogOutOutline /></button>
+                </div>
+                <div id="description">
+                    <p>Informe-se sobre:</p>
+                </div>
+                <nav id="navigation-home">
+                    <ul>
+                        <li onClick={() => changeComponent('Dashboard')}>
+                            <span className="icon"><IoGridOutline /></span>
+                            <span className="title-home">
+                                <h4>DashBoard</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('Produtos')}>
+                            <span className="icon"><IoBasketOutline /></span>
+                            <span className="title-home">
+                                <h4>Produtos</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('Regioes')}>
+                            <span className="icon"><IoLocationOutline /></span>
+                            <span className="title-home">
+                                <h4>Regiões</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('Funcionarios')}>
+                            <span className="icon"><IoPeopleOutline /></span>
+                            <span className="title-home">
+                                <h4>Funcionários</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('Financeiro')}>
+                            <span className="icon"><IoBarChartOutline /></span>
+                            <span className="title-home">
+                                <h4>Financeiro</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('Relatorio')}>
+                            <span className="icon"><IoDocumentTextOutline /></span>
+                            <span className="title-home">
+                                <h4>Enviar Relatório</h4>
+                            </span>
+                        </li>
+                        <li onClick={() => changeComponent('FAQ')}>
+                            <span className="icon"><IoPaperPlaneOutline /></span>
+                            <span className="title-home">
+                                <h4>Fale conosco</h4>
+                            </span>
+                        </li >
+                    </ul>
+                </nav>
+            </aside>
+        </main>
+    )
 }
 
 export default Aside
