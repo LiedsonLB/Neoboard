f6cb2cd451aa73a889188439e4e7ce69b6df4841
DouglasSantos777 <fdsantosdec@aluno.uespi.br>
2024-03-23 23:11:27 -0300

    fix: reponsive in faq and calendar

--- src/layout/FAQ/FAQ.tsx
+++ src/layout/FAQ/FAQ.tsx
@@ -6,17 +6,17 @@ import "./FAQ.css";
 
 import { IoChevronForwardOutline, IoLogoWhatsapp, IoLogoLinkedin, IoMail } from 'react-icons/io5';
 
- 
+
 
 const FAQ = () => {
 
- 
+
 
   function sendEmail(e: any) {
 
     e.preventDefault();
 
- 
+
 
     const templateParams = {
 
@@ -30,7 +30,7 @@ const FAQ = () => {
 
     };
 
- 
+
 
     emailjs.send('service_2fh7kiu', 'template_1dwajhz', templateParams, 'ytmT8nY7U6e4AGqet')
 
@@ -38,186 +38,182 @@ const FAQ = () => {
 
         console.log(result.text);
 
-        // lógica para mostrar um popup de sucesso
-
       }, (error) => {
 
         console.log(error.text);
 
-        // lógica para mostrar um popup de erro
-
       });
 
- 
+
 
     e.target.reset();
 
   }
 
- 
+
 
   return (
 
     <main id='faq-container'>
+      <div id='faq-inside'>
+        <header id='faq-header'>
 
-      <header id='faq-header'>
+          <span>
 
-        <span>
+            <h1>Entre em contato conosco</h1>
 
-          <h1>Entre em contato conosco</h1>
+          </span>
 
-        </span>
+        </header>
 
-      </header>
 
- 
 
-      <div id='faq-sections'>
+        <div id='faq-sections'>
 
-        <section id='faq-left'>
+          <section id='faq-left'>
 
-          <header>
+            <header>
 
-            <h2>Fale conosco</h2>
+              <h2>Fale conosco</h2>
 
-          </header>
+            </header>
 
-          <div id='faq-cards'>
+            <div id='faq-cards'>
 
-            <div className='social-card'>
+              <div className='social-card'>
 
-              <span className='icon-faq-email'><IoMail /></span>
+                <span className='icon-faq-email'><IoMail /></span>
 
-              <span><p className='faq-contacts'>Email</p></span>
+                <span><p className='faq-contacts'>Email</p></span>
 
-              <span><p>neoboard@neo.com</p></span>
+                <span><p>neoboard@neo.com</p></span>
 
-              <a href="mailto:liedson.b9@gmail.com">Escrever Email<IoChevronForwardOutline /></a>
-
-            </div>
-
- 
-
-            <form onSubmit={sendEmail} className='social-card'>
-
-              <span className='icon-faq-zap'><IoLogoWhatsapp /></span>
-
-              <span><p className='faq-contacts'>Whatsapp</p></span>
-
-              <span><p>+5586998635571</p></span>
-
-              <a href="https://wa.me/86998635571">Conversar no Whatsapp<IoChevronForwardOutline /></a>
-
-            </form>
-
- 
-
-            <div className='social-card'>
-
-              <span className='icon-faq-linkedin'><IoLogoLinkedin /></span>
-
-              <span><p className='faq-contacts'>Linkedin</p></span>
-
-              <span><p>in/neoboard</p></span>
-
-              <a href="https://www.linkedin.com/in/liedsonlb/">Contato no Linkedin<IoChevronForwardOutline /></a>
-
-            </div>
-
-          </div>
-
-        </section>
-
- 
-
-        <section id='faq-right'>
-
-          <header>
-
-            <h2>Descreva seu interesse</h2>
-
-          </header>
-
-          <div id='faq-field'>
-
-            <form onSubmit={sendEmail}>
-
-              <div className="input-faq">
-
-                <label>Nome:</label>
-
-                <input
-
-                  type="text"
-
-                  name="user_name"
-
-                  placeholder="Seu nome"
-
-                  className="data-faq"
-
-                />
+                <a href="mailto:liedson.b9@gmail.com">Escrever Email<IoChevronForwardOutline /></a>
 
               </div>
 
-              <div className="input-faq">
 
-                <label>Email:</label>
 
-                <input
+              <form onSubmit={sendEmail} className='social-card'>
 
-                  type="email"
+                <span className='icon-faq-zap'><IoLogoWhatsapp /></span>
 
-                  name="user_email"
+                <span><p className='faq-contacts'>Whatsapp</p></span>
 
-                  placeholder="Seu email favorito"
+                <span><p>+5586998635571</p></span>
 
-                  className="data-faq"
+                <a href="https://wa.me/86998635571">Conversar no Whatsapp<IoChevronForwardOutline /></a>
 
-                />
+              </form>
+
+
+
+              <div className='social-card'>
+
+                <span className='icon-faq-linkedin'><IoLogoLinkedin /></span>
+
+                <span><p className='faq-contacts'>Linkedin</p></span>
+
+                <span><p>in/neoboard</p></span>
+
+                <a href="https://www.linkedin.com/in/liedsonlb/">Contato no Linkedin<IoChevronForwardOutline /></a>
 
               </div>
 
-              <div className="input-faq-msg">
+            </div>
 
-                <label>Mensagem:</label>
+          </section>
 
-                <div className='faq-msg'>
 
-                  <textarea
 
-                    name="message"
+          <section id='faq-right'>
 
-                    placeholder="Envie sua mensagem..."
+            <header>
 
-                    className="msg-faq"
+              <h2>Descreva seu interesse</h2>
+
+            </header>
+
+            <div id='faq-field'>
+
+              <form onSubmit={sendEmail}>
+
+                <div className="input-faq">
+
+                  <label>Nome:</label>
+
+                  <input
+
+                    type="text"
+
+                    name="user_name"
+
+                    placeholder="Seu nome"
+
+                    className="data-faq"
 
                   />
 
                 </div>
 
-              </div>
+                <div className="input-faq">
 
-              <button type="submit" className="faq-btn">
+                  <label>Email:</label>
 
-                Enviar
+                  <input
 
-              </button>
+                    type="email"
 
-            </form>
+                    name="user_email"
 
-          </div>
+                    placeholder="Seu email favorito"
 
-        </section>
+                    className="data-faq"
 
+                  />
+
+                </div>
+
+                <div className="input-faq-msg">
+
+                  <label>Mensagem:</label>
+
+                  <div className='faq-msg'>
+
+                    <textarea
+
+                      name="message"
+
+                      placeholder="Envie sua mensagem..."
+
+                      className="msg-faq"
+
+                    />
+
+                  </div>
+
+                </div>
+
+                <button type="submit" className="faq-btn">
+
+                  Enviar
+
+                </button>
+
+              </form>
+
+            </div>
+
+          </section>
+
+        </div>
       </div>
-
     </main>
 
   )
 
 }
 
- 
+
 
 export default FAQ;
