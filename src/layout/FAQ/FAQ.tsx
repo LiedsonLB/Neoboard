393f01efa37d498515ebaded1189d347ed7c3923
DouglasSantos777 <fdsantosdec@aluno.uespi.br>
2024-03-28 23:22:19 -0300

    feat: add product page

--- src/layout/FAQ/FAQ.tsx
+++ src/layout/FAQ/FAQ.tsx
@@ -4,7 +4,7 @@ import "./FAQ.css";
 import { IoChevronForwardOutline, IoLogoWhatsapp, IoLogoLinkedin, IoMail } from 'react-icons/io5';
 
 const FAQ = () => {
-  
+
   function sendEmail(e: any) {
     e.preventDefault();
 
@@ -28,81 +28,83 @@ const FAQ = () => {
 
   return (
     <main id='faq-container'>
-      <header id='faq-header'>
-        <span>
-          <h1>Entre em contato conosco</h1>
-        </span>
-      </header>
+      <div id='faq-inside'>
+        <header id='faq-header'>
+          <span>
+            <h1>Entre em contato conosco</h1>
+          </span>
+        </header>
 
-      <div id='faq-sections'>
-        <section id='faq-left'>
-          <header>
-            <h2>Fale conosco</h2>
-          </header>
-          <div id='faq-cards'>
-            <div className='social-card'>
-              <span className='icon-faq-email'><IoMail /></span>
-              <span><p className='faq-contacts'>Email</p></span>
-              <span><p>neoboard@neo.com</p></span>
-              <a href="mailto:liedson.b9@gmail.com">Escrever Email<IoChevronForwardOutline /></a>
-            </div>
-
-            <form onSubmit={sendEmail} className='social-card'>
-              <span className='icon-faq-zap'><IoLogoWhatsapp /></span>
-              <span><p className='faq-contacts'>Whatsapp</p></span>
-              <span><p>+5586998635571</p></span>
-              <a href="https://wa.me/86998635571">Conversar no Whatsapp<IoChevronForwardOutline /></a>
-            </form>
-
-            <div className='social-card'>
-              <span className='icon-faq-linkedin'><IoLogoLinkedin /></span>
-              <span><p className='faq-contacts'>Linkedin</p></span>
-              <span><p>in/neoboard</p></span>
-              <a href="https://www.linkedin.com/in/liedsonlb/">Contato no Linkedin<IoChevronForwardOutline /></a>
-            </div>
-          </div>
-        </section>
-
-        <section id='faq-right'>
-          <header>
-            <h2>Descreva seu interesse</h2>
-          </header>
-          <div id='faq-field'>
-            <form onSubmit={sendEmail}>
-              <div className="input-faq">
-                <label>Nome:</label>
-                <input
-                  type="text"
-                  name="user_name"
-                  placeholder="Seu nome"
-                  className="data-faq"
-                />
+        <div id='faq-sections'>
+          <section id='faq-left'>
+            <header>
+              <h2>Fale conosco</h2>
+            </header>
+            <div id='faq-cards'>
+              <div className='social-card'>
+                <span className='icon-faq-email'><IoMail /></span>
+                <span><p className='faq-contacts'>Email</p></span>
+                <span><p>neoboard@neo.com</p></span>
+                <a href="mailto:liedson.b9@gmail.com">Escrever Email<IoChevronForwardOutline /></a>
               </div>
-              <div className="input-faq">
-                <label>Email:</label>
-                <input
-                  type="email"
-                  name="user_email"
-                  placeholder="Seu email favorito"
-                  className="data-faq"
-                />
+
+              <form onSubmit={sendEmail} className='social-card'>
+                <span className='icon-faq-zap'><IoLogoWhatsapp /></span>
+                <span><p className='faq-contacts'>Whatsapp</p></span>
+                <span><p>+5586998635571</p></span>
+                <a href="https://wa.me/86998635571">Conversar no Whatsapp<IoChevronForwardOutline /></a>
+              </form>
+
+              <div className='social-card'>
+                <span className='icon-faq-linkedin'><IoLogoLinkedin /></span>
+                <span><p className='faq-contacts'>Linkedin</p></span>
+                <span><p>in/neoboard</p></span>
+                <a href="https://www.linkedin.com/in/liedsonlb/">Contato no Linkedin<IoChevronForwardOutline /></a>
               </div>
-              <div className="input-faq-msg">
-                <label>Mensagem:</label>
-                <div className='faq-msg'>
-                  <textarea
-                    name="message"
-                    placeholder="Envie sua mensagem..."
-                    className="msg-faq"
+            </div>
+          </section>
+
+          <section id='faq-right'>
+            <header>
+              <h2>Descreva seu interesse</h2>
+            </header>
+            <div id='faq-field'>
+              <form onSubmit={sendEmail}>
+                <div className="input-faq">
+                  <label>Nome:</label>
+                  <input
+                    type="text"
+                    name="user_name"
+                    placeholder="Seu nome"
+                    className="data-faq"
                   />
                 </div>
-              </div>
-              <button type="submit" className="faq-btn">
-                Enviar
-              </button>
-            </form>
-          </div>
-        </section>
+                <div className="input-faq">
+                  <label>Email:</label>
+                  <input
+                    type="email"
+                    name="user_email"
+                    placeholder="Seu email favorito"
+                    className="data-faq"
+                  />
+                </div>
+                <div className="input-faq-msg">
+                  <label>Mensagem:</label>
+                  <div className='faq-msg'>
+                    <textarea
+                      name="message"
+                      placeholder="Envie sua mensagem..."
+                      className="msg-faq"
+                    />
+                  </div>
+                </div>
+                <button type="submit" className="faq-btn">
+                  Enviar
+                </button>
+              </form>
+            </div>
+          </section>
+        </div>
       </div>
     </main>
   )
--- src/layout/produtos/Produtos.tsx
+++ src/layout/produtos/Produtos.tsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import "./Produtos.css";
+import { IoChevronForwardOutline, IoLogoWhatsapp, IoLogoLinkedin, IoMail, IoSearch, IoSearchCircle, IoSearchCircleOutline, IoSearchSharp } from 'react-icons/io5';
+import { FaDivide } from 'react-icons/fa';
 
 const Produtos = () => {
   return (
-    <div>Página Produtos</div>
+    <div id='product-container'>
+     <div id='product-inside'>
+      <header id="prod-header">
+        <h1>Produtos</h1>
+        <p>Confira os produtos que mais venderam</p>
+      </header>
+
+      <main id='product-main'>
+        <article id='prod-main'>
+          <div id='container-prod-img'>
+            <figure className='icecream-img'>
+              <img src="./img/picole.jpg" alt="picole_flocos" />
+            </figure>
+            <div id='prod-desc'>
+              <h1>Picolé</h1>
+              <p>Faturamento:</p>
+              <p>Unidades vendidas</p>
+            </div>
+          </div>
+
+          <div id='card-prod-desc'>
+            <p>Produto do Mês</p>
+            <div id='product-chart'>
+            </div>
+          </div>
+
+        </article>
+
+        <section id='search-prod'>
+          <div id='search-bar'>
+            <input type="search" id="search-product" placeholder='Pesquisar produto' aria-label="Buscar" />
+            <i id='search-icon'><IoSearch id='icon-prod' /></i>
+          </div>
+
+          <button id='add-product'>
+            + Produto
+          </button>
+        </section>
+
+        <section id='products-list'>
+          <article className='prod-card'>
+            <figure className='icecream-img'>
+              <img src="./img/picole.jpg" alt="picole_flocos" />
+            </figure>
+            <p>Picolé de flocos de 20ml</p>
+            <p className='prod-name'>R$ 2.50</p>
+            <button>Ver produto</button>
+          </article>
+
+          <article className='prod-card'>
+            <figure className='icecream-img'>
+              <img src="./img/picole.jpg" alt="picole_flocos" />
+            </figure>
+            <p>Picolé de flocos de 20ml</p>
+            <p className='prod-name'>R$ 2.50</p>
+            <button>Ver produto</button>
+          </article>
+
+          <article className='prod-card'>
+            <figure className='icecream-img'>
+              <img src="./img/picole.jpg" alt="picole_flocos" />
+            </figure>
+            <p>Picolé de flocos de 20ml</p>
+            <p className='prod-name'>R$ 2.50</p>
+            <button>Ver produto</button>
+          </article>
+
+          <article className='prod-card'>
+            <figure className='icecream-img'>
+              <img src="./img/picole.jpg" alt="picole_flocos" />
+            </figure>
+            <p>Picolé de flocos de 20ml</p>
+            <p className='prod-name'>R$ 2.50</p>
+            <button>Ver produto</button>
+          </article>
+
+        </section>
+      </main>
+      </div>
+    </div>
   )
 }
 
