a2b8a8786b4a1d7fe66297f792bbde8bf7937f72
LiedsonLB <liedson.b9@gmail.com>
2024-04-14 20:57:50 -0300

    feat: error page

--- src/pages/errorpage/Errorpage.tsx
+++ src/pages/errorpage/Errorpage.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../errorpage/Errorpage.css'
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 - Página Não Encontrada</h1>
-      <p>A página que você está procurando pode ter sido removida, renomeada ou está temporariamente indisponível.</p>
-      <p>Por favor, verifique o URL novamente ou retorne à página inicial.</p>
-      <Link to='/' style={{color: 'var(--primary-color)', marginTop:'5px'}}>Voltar para Home</Link>
+    <div id="screen-error">
+      <div id="container-error">
+        <div id="img-error">
+          <img src="/img/avatarHomeSkills.png" alt="" />
+        </div>
+        <div id="text-error">
+          <h1>ERROR <span>404</span>:<br className='br-error' /> Página não encontrada</h1>
+          <p>Nosso programador Neo dormiu no teclado, e a página fugiu! Enquanto o café faz efeito, explore outras partes do nosso universo digital na página inicial. Agradecemos pela paciência!</p>
+          <Link to='/' style={{ color: 'var(--secondy-color)', marginTop: '5px' }}>Voltar para Home</Link>
+        </div>
+      </div>
     </div>
   )
 }
