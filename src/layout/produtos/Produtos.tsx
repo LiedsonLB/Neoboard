d70f9c606b3b9246bb87334c6e2761ee6482050f
LiedsonLB <liedson.b9@gmail.com>
2024-04-26 01:35:13 -0300

    realese v2.0.0

--- src/layout/produtos/Produtos.tsx
+++ src/layout/produtos/Produtos.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "./Produtos.css";
-import { IoSearch, IoCamera, IoPencil, IoTrash, IoPencilSharp, IoCreate, IoMagnetOutline } from 'react-icons/io5';
+import { IoSearch, IoCamera, IoPencil, IoTrash, IoCreate } from 'react-icons/io5';
 import ProductDoughnut from '../../components/charts/ProductDoughtnout';
 import ProductColumnChart from '../../components/charts/ProductColumnChart.tsx';
 import axios from 'axios';
@@ -8,11 +8,14 @@ import axios from 'axios';
 const Produtos = () => {
   const [showModal, setShowModal] = useState(false);
   const [showInfoModal, setShowInfoModal] = useState(false);
-  const [showNeoModal, setShowNeoModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [categoriaSelecionada, setCategoriaSelecionada] = useState('');
   const [produtos, setProdutos] = useState([]);
   const [filtroPesquisa, setFiltroPesquisa] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState<any>(null);
+  const [valorFormatado, setValorFormatado] = useState<string>('');
+  const [valorAcumulado, setValorAcumulado] = useState('');
+  const [categorias, setCategorias] = useState<string[]>([]);
 
   const toggleModalClose = () => {
     setShowModal(!showModal);
@@ -32,9 +35,12 @@ const Produtos = () => {
 
   const fetchProdutos = async () => {
     try {
-      const response = await axios.get('http://localhost:4000/v3/produtos');
+      const response = await axios.get('http://localhost:4000/v2/produtos');
       setProdutos(response.data);
       console.log(response.data)
+      const categoriasUnicas = new Set(response.data.map((produto: any) => produto.categoria));
+      const categoriasUnicasArray: string[] = Array.from(categoriasUnicas);
+      setCategorias(categoriasUnicasArray);
     } catch (error) {
       console.error('Erro ao buscar produtos:', error);
     }
@@ -75,20 +81,20 @@ const Produtos = () => {
           const novoProduto = {
             nome,
             categoria,
-            preco: valor, // Renomear para 'preco' conforme a estrutura dos produtos
+            preco: valor,
             descricao,
-            imagem: selectedImage ? selectedImage : './img/no_productImg.jpeg', // Renomear para 'imagem' conforme a estrutura dos produtos
+            picture: selectedImage ? selectedImage : './img/no_productImg.jpeg',
           };
 
-          // Limpar os campos de entrada após adicionar o produto
           nomeElement.value = '';
           categoriaElement.value = '';
           valorElement.value = '';
           descricaoElement.value = '';
+          setSelectedImage(null);
+          setShowModal(false);
 
-          await axios.post('http://localhost:4000/v3/produtos', novoProduto);
-          fetchProdutos(); // Atualizar a lista de produtos após adicionar um novo
-          setShowModal(false); // Fechar o modal de adicionar produto
+          await axios.post('http://localhost:4000/v2/produtos', novoProduto);
+          fetchProdutos();
         } else {
           console.error('Erro ao adicionar produto: Algum campo não foi preenchido.');
         }
@@ -100,8 +106,27 @@ const Produtos = () => {
     }
   };
 
-  const filtrarPorCategoria = (categoria: string) => {
-    setCategoriaSelecionada(categoria);
+  const handleValorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let valorDigitado = e.target.value;
+
+    valorDigitado = valorDigitado.replace(/[^\d.]/g, '');
+
+    if (!valorDigitado) {
+      setValorAcumulado('');
+      setValorFormatado(''); // Adicione esta linha para limpar o valor formatado quando não houver entrada
+      return;
+    }
+
+    const novoValorAcumulado = valorAcumulado + valorDigitado;
+
+    const reais = Math.floor(parseInt(novoValorAcumulado) / 100);
+    const centavos = parseInt(novoValorAcumulado) % 100;
+
+    const valorFormatado = `R$ ${reais}.${centavos}`;
+
+    setValorAcumulado(valorAcumulado + valorDigitado);
+    setValorFormatado(valorFormatado); // Atualize o estado valorFormatado aqui
+    console.log(valorFormatado)
   };
 
   const produtosFiltrados = produtos.filter((produto: any) =>
@@ -113,87 +138,13 @@ const Produtos = () => {
     setFiltroPesquisa(e.target.value);
   };
 
-  const toggleNeoModalOpen = () => {
-    setShowNeoModal(true);
-    const helpElement = document.getElementById('Neo-Help');
-    if (helpElement) {
-      helpElement.style.display = 'none';
-    }
-  };
-
-  const toggleNeoModalClose = () => {
-    setShowNeoModal(false);
-    const neoElement = document.getElementById('Neo-Help');
-    if (neoElement && neoElement.style.display === 'none') {
-      neoElement.style.display = 'block';
-      neoElement.style.animation = 'none';
-    }
+  const handleShowInfoModal = (produto: any) => {
+    setSelectedProduct(produto);
+    setShowInfoModal(true);
   };
 
   return (
     <>
-      {showNeoModal && <div className="Modal-Help">
-        <div className='container-Help'>
-          <div className="header-Help">
-            <h4 className="help-title">Dicas do Neo: </h4>
-            <button type="button" className="close-btn" onClick={toggleNeoModalClose}>&times;</button>
-          </div>
-
-          <div className='Help-Info'>
-            <div className='Help-Text'>
-              <div id='Neo-Text'>
-                <h1>Olá, eu sou o <span>Neo </span>!</h1>
-                <img src="./img/NeoL.png" alt="Neo-Sit" />
-                <p>Serei o seu guia do NeoBoard, sempre que precisar de mim, clique em meu ícone na lateral <span className='span-right'>direita </span>.
-                  Atualmente você está na seção principal do NeoBoard, aqui vai um breve resumo de cada trecho desta página.</p>
-              </div>
-              <ul>
-                <li>
-                  <h4>Selecione o Período:</h4>
-                  <img className='help-imgs' src="./img/periodsimg.png" alt="periodo" />
-                  <p>Os dados da página podem ser selecionados de acordo com o período que você desejar.</p>
-                </li>
-
-                <li>
-                  <h4>Cartões Financeiros:</h4>
-                  <img className='help-imgs' src="./img/cardsfatura.png" alt="faturamento" />
-                  <p>Números importantes, como o faturamento, despesas e lucro.</p>
-                </li>
-
-                <li>
-                  <h4>Gráficos de Arrecadação:</h4>
-                  <img className='help-imgs' src="./img/arrecada.png" alt="arrecadação" />
-                  <p>Explore os gráficos de arrecadação para entender melhor a distribuição da sua receita. Os gráficos de linha e de rosca oferecem um panorama sobre padrões e tendências de arrecadação.</p>
-                </li>
-
-                <li>
-                  <h4>Ranking de Desempenho:</h4>
-                  <img className='help-imgs' src="./img/rankingimg.png" alt="ranking" />
-                  <p>Descubra quais produtos estão se destacando em vendas, a arrecadação por região e o desempenho dos funcionários.. </p>
-                </li>
-
-                <li>
-                  <h4>Dados de Pagamento e Despesas:</h4>
-                  <img className='help-imgs' src="./img/pagamentosimg.png" alt="pagamento" />
-                  <p>Veja as formas de pagamento utilizadas pelos clientes, juntamente com um gráfico que ilustra a distribuição dessas formas. </p>
-                </li>
-
-                <li>
-                  <h4>Dados de Pagamento e Despesas:</h4>
-                  <img className='help-imgs' src="./img/Despesasimg.png" alt="despesas" />
-                  <p>Analise de forma geral as suas despesas com um gráfico de coluna, além disso o NeoBoard oferece um calendário para registrar as despesas de determinado dia. </p>
-                </li>
-              </ul>
-              <button onClick={toggleNeoModalClose} className='help-btn'>Entendi</button>
-            </div>
-
-            <figure className='Neo-Left'>
-              <img src="./img/NeoL.png" alt="Neo-Sit" />
-            </figure>
-          </div>
-        </div>
-      </div>}
-
       {showModal && (
         <div className="Modal-Add">
           <div className='container-Add'>
@@ -232,7 +183,7 @@ const Produtos = () => {
 
                 <span>
                   <label htmlFor="valor-item">Valor Unitário (R$):</label>
-                  <input type="text" id='valor-item' name='valor-item' className='full-item' />
+                  <input type="text" id='valor-item' name='valor-item' className='full-item' onChange={handleValorChange} value={valorFormatado} />
                 </span>
               </div>
 
@@ -247,7 +198,7 @@ const Produtos = () => {
                 </span>
               </div>
 
-              <button id='add-staff-Btn' onClick={adicionarProduto}>Enviar</button>
+              <button id='add-staff-Btn' onClick={() => { adicionarProduto(); setShowModal(false) }}>Enviar</button>
             </div>
           </div>
         </div>
@@ -256,6 +207,7 @@ const Produtos = () => {
       {showInfoModal && (
         <div className="Modal-Add">
           <div className="container-Detail-Product">
+
             <div id="header-modal">
               <h4 className="modal-title">Informações do Produto</h4>
               <button type="button" className="close-btn" onClick={() => setShowInfoModal(false)}>&times;</button>
@@ -264,12 +216,12 @@ const Produtos = () => {
             <div id='Product-Info-Container'>
               <div id='infoprod-popup'>
                 <div id='prodInfo-popup'>
-                  <img src='/img/no_productImg.jpeg' alt="product-avatar" />
-                  <h2 className='nameUserProd'> Picolé de Flocos</h2>
+                  <img src={selectedProduct.picture} alt="product-avatar" />
+                  <h2 className='nameUserProd'>{selectedProduct.nome}</h2>
                   <div id="ProdTextInfo">
-                    <p><span>Categoria:</span> Picolé</p>
-                    <p><span>Valor:</span> R$ 2,50</p>
-                    <p><span>Descrição:</span> Picolé muito bala mesmo </p>
+                    <p><span>Categoria:</span> {selectedProduct.categoria}</p>
+                    <p><span>Valor:</span> R$ {selectedProduct.preco}</p>
+                    <p><span>Descrição:</span> {selectedProduct.descricao}</p>
                   </div>
                 </div>
               </div>
@@ -315,13 +267,9 @@ const Produtos = () => {
               </div>
 
             </div>
-          </div>
-        </div>
+          </div >
+        </div >
       )}
-
-      <div id='Neo-Help' onClick={toggleNeoModalOpen}>
-        <img src="/img/NeoHead.png" alt="neo_head" />
-      </div>
 
       <div id='product-container'>
         <div id='product-inside'>
@@ -331,7 +279,7 @@ const Produtos = () => {
           </header>
 
           <main id='product-main'>
-            <article id='product-card'>
+            <article id='prod-card'>
               <p id='text-prod-mes'>Produto do Mês</p>
               <div id='prod-main'>
                 <div id='container-prod-img'>
@@ -355,9 +303,19 @@ const Produtos = () => {
                 <input type="search" id="search-product" placeholder='Pesquisar produto' aria-label="Buscar" onChange={handleFiltroChange} />
                 <i id='search-icon'><IoSearch id='icon-prod' /></i>
               </div>
-              <button id='add-product' onClick={toggleModalClose}>
-                + Produto
-              </button>
+
+              <div className='filter-container-btns'>
+                <select id="filter-expense" value={categoriaSelecionada} onChange={(e) => setCategoriaSelecionada(e.target.value)}>
+                  <option value="">Todos</option>
+                  {categorias.map((categoria, index) => (
+                    <option key={index} value={categoria}>{categoria}</option>
+                  ))}
+                </select>
+
+                <button id='add-product' onClick={toggleModalClose}>
+                  + Produto
+                </button>
+              </div>
             </section>
 
             <p id='result-product'>Resultados ({produtosFiltrados.length})</p>
@@ -366,11 +324,11 @@ const Produtos = () => {
               {produtosFiltrados.map((produto: any) => (
                 <article key={produto.id} className='prod-card'>
                   <figure className='container-list-img'>
-                    <img src={produto.imagem} alt={produto.nome} />
+                    <img src={produto.picture} alt={produto.nome} />
                   </figure>
                   <p>{produto.nome}</p>
                   <p className='prod-name'>R$ {produto.preco}</p>
-                  <button className='see-prod-btn' onClick={() => setShowInfoModal(true)}>Ver produto</button>
+                  <button className='see-prod-btn' onClick={() => { setShowInfoModal(true); setSelectedProduct(produto) }}>Ver produto</button>
                   <div className='manager-btn'>
                     <div>
                       <button className='edit-item item-mng'><IoCreate id='edit-pen' /></button>
@@ -384,7 +342,7 @@ const Produtos = () => {
             </section>
           </main>
         </div>
-      </div>
+      </div >
     </>
   );
 }
